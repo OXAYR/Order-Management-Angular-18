9bea22eb29a18f6dcdc8e741240f3bbda5a399d1
Oxayr <uzairmaqsood451@gmail.com>
2024-10-08 14:30:35 +0500

    added master component to manage child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RolesComponent } from './components/roles/roles.component';
-import { DesignationComponent } from './components/designation/designation.component';
+import { MasterComponent } from './components/master/master.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RolesComponent, DesignationComponent],
+  imports: [RouterOutlet, MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RolesComponent } from '../roles/roles.component';
+import { DesignationComponent } from '../designation/designation.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [],
+  imports: [RolesComponent, DesignationComponent],
   templateUrl: './master.component.html',
-  styleUrl: './master.component.css'
+  styleUrl: './master.component.css',
 })
-export class MasterComponent {
-
-}
+export class MasterComponent {}
