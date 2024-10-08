c147b617f07ee6c3b95785f7cdc2b6b228e4d437
Oxayr <uzairmaqsood451@gmail.com>
2024-10-08 13:54:16 +0500

    made the initial types

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+// componennt decorator: that it will provide the information about the class
+
 @Component({
   selector: 'app-roles',
   standalone: true,
   imports: [],
   templateUrl: './roles.component.html',
-  styleUrl: './roles.component.css'
+  styleUrl: './roles.component.css',
 })
 export class RolesComponent {
-
+  firstName: string = 'My First Angular Project'; //strongly typed data
+  angularVersion = 'Version 18'; // losesly typed data
+  version: number = 18;
+  isActive: Boolean = true;
+  currentDate: Date = new Date();
 }
